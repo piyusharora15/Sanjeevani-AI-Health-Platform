d915463d90e546761e0972503c340fe83c180572
Piyush Arora <piyusharora1504@gmail.com>
2025-07-30 21:44:14 +0530

    added animation to make home page intuitive

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,20 @@ export default { // Use "export default"
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      keyframes: {
+        slideInUp: {
+          '0%': { transform: 'translateY(30px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideInLeft: {
+          '0%': { transform: 'translateX(-30px)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideInUp: 'slideInUp 0.7s ease-out forwards',
+        slideInLeft: 'slideInLeft 0.7s ease-out forwards',
+      }
     },
   },
   plugins: [],
