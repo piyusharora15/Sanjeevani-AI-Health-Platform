93c703c7b53ced44dc5c1c030060e29d3c520d1c
Piyush Arora <piyusharora1504@gmail.com>
2025-08-04 22:42:43 +0530

    added animations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,10 +18,20 @@ export default { // Use "export default"
           '0%': { transform: 'translateX(-30px)', opacity: '0' },
           '100%': { transform: 'translateX(0)', opacity: '1' },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        zoomIn: {
+          '0%': { transform: 'scale(0.95)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        }
       },
       animation: {
         slideInUp: 'slideInUp 0.7s ease-out forwards',
         slideInLeft: 'slideInLeft 0.7s ease-out forwards',
+        fadeIn: 'fadeIn 1s ease-in-out forwards',
+        zoomIn: 'zoomIn 0.6s ease-out forwards',
       }
     },
   },
