eb361bf2621ef84c9b963f094fe3e658595b1ca0
Piyush Arora <piyusharora1504@gmail.com>
2025-08-13 21:26:32 +0530

    routes for appointment booking

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { bookAppointment } = require('../controllers/appointmentController');
+const { bookAppointment,getMyBookingsAsPatient,getMyBookingsAsDoctor } = require('../controllers/appointmentController');
 const { protect } = require('../middleware/authMiddleware');
 
 // This route is protected. A user must be logged in to book an appointment.
 // The `protect` middleware will run first to verify the user and attach their info to `req.user`.
 router.post('/', protect, bookAppointment);
+
+// Route for a patient to get their bookings ---
+router.get('/mypatient', protect, getMyBookingsAsPatient);
+
+// Route for a doctor to get their bookings ---
+router.get('/mydoctor', protect, getMyBookingsAsDoctor);
 
 module.exports = router;
