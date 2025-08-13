ed6b227aacc2b12a0587f266b90eb127bab1019e
Piyush Arora <piyusharora1504@gmail.com>
2025-08-13 21:26:03 +0530

    added user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,21 @@
-const getUserProfile = async (req, res) => {
-  // Because our `protect` middleware ran first, the user object is already attached to the request (req.user)
-  const user = await User.findById(req.user._id);
-
-  if (user) {
+const user = require('../models/User');
+// @desc    Get current user's profile
+// @route   GET /api/users/profile/me
+// @access  Private
+const getMyUserProfile = async (req, res) => {
+  // The `protect` middleware already found the user and attached it to req.user
+  // We don't need to find them again. Just send back the data.
+  if (req.user) {
     res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
+      _id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+      role: req.user.role,
+      createdAt: req.user.createdAt,
     });
   } else {
     res.status(404).json({ message: 'User not found' });
   }
 };
 
-module.exports = { getUserProfile };
+module.exports = { getMyUserProfile };
