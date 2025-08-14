ad034f8b2e072e85915a7e91ca33b57b9baee73c
Piyush Arora <piyusharora1504@gmail.com>
2025-08-14 22:22:02 +0530

    routes for doctor profile

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createOrUpdateDoctorProfile,getAllDoctors } = require('../controllers/doctorController');
+const { createOrUpdateDoctorProfile,getAllDoctors,getMyDoctorProfile } = require('../controllers/doctorController');
 const { protect } = require('../middleware/authMiddleware');
 
 // This route is protected, so the user must be logged in.
 // The `protect` middleware will run first, followed by the controller function.
 // Route for doctors to create/update their profile (Private)
+
+router.get('/profile/me', protect, getMyDoctorProfile); // Get the logged-in doctor's profile
 router.post('/profile', protect, createOrUpdateDoctorProfile);
  // Route for anyone to get a list of all doctors (Public) ---
 router.get('/', getAllDoctors);
