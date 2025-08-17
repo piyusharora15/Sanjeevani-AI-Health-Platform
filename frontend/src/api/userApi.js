06756dc0663e15fc720e5dc99ff3611f6e3aa4c4
Piyush Arora <piyusharora1504@gmail.com>
2025-08-17 15:39:03 +0530

    update api url

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/users';
+const API_URL = 'https://sanjeevani-api.onrender.com/api/users';
 
 // Function to get the logged-in user's profile
 export const getMyProfile = async (token) => {
