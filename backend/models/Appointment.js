08694d9675a3b96a2bd61e8fd663faa5e141b559
Piyush Arora <piyusharora1504@gmail.com>
2026-05-10 12:52:29 +0530

    refactor: migrate Appointment.js to ES module syntax

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const appointmentSchema = new mongoose.Schema(
   {
@@ -47,4 +47,4 @@ const appointmentSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Appointment", appointmentSchema);
+export default mongoose.model("Appointment", appointmentSchema);
