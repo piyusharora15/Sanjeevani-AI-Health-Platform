9e229cf83f3229a1ab4cf277bf1779f809585f45
Piyush Arora <piyusharora1504@gmail.com>
2026-05-10 12:54:29 +0530

    refactor: migrate assistantRoutes.js to ES module syntax

--- backend/routes/assistantRoutes.js
+++ backend/routes/assistantRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-const { processSymptoms } = require('../controllers/assistantController');
-const { protect } = require('../middleware/authMiddleware');
+const { processSymptoms } = import('../controllers/assistantController');
+const { protect } = import('../middleware/authMiddleware');
 
 // This is a protected route. A user must be logged in to access it.
 // The `protect` middleware will run first to verify the user's token.
 router.post('/symptoms', protect, processSymptoms);
 
-module.exports = router;
+export default router;
