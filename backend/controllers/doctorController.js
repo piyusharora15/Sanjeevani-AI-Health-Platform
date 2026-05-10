eed1f3727994d463240ab4f25e9cf1fd553b9c5f
Piyush Arora <piyusharora1504@gmail.com>
2026-05-10 14:34:02 +0530

    refactor: update doctorController.js to use named imports for Doctor and User models

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,5 +1,5 @@
-import Doctor from "../models/Doctor.js";
-import User from "../models/User.js";
+import { Doctor } from "../models/Doctor.js";
+import { User } from "../models/User.js";
 
 // @desc    Create or update a doctor's profile
 // @route   POST /api/doctors/profile
